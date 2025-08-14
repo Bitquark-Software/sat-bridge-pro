0916ee9811a01331137634a03b3caf37bc0b4512
Ing. Fernando <idsfernandomorales@gmail.com>
2025-08-14 13:17:23 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubPages = process.env.NODE_ENV === 'production'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: isGitHubPages ? '/sat-bridge-pro/' : '/',
 })
