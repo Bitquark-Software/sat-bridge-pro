d569a13413e762db04d9613846402b0912b09c4d
Ing. Fernando <idsfernandomorales@gmail.com>
2025-08-14 13:19:32 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isGitHubPages = process.env.NODE_ENV === 'production'
+const isGitHubPages = import.meta.env.NODE_ENV === 'production'
 
 // https://vite.dev/config/
 export default defineConfig({
