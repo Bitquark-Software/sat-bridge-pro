751c95595a9a4117f1e2ffb5b765e36e04f8e547
Ing. Fernando <idsfernandomorales@gmail.com>
2025-08-14 13:52:28 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const isLocalDevelopment = process.env.NODE_ENV === 'local'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isLocalDevelopment ? '/ ' : './',
+  base: isLocalDevelopment ? '/' : './',
   build: {
     outDir: 'build', // <-- genera directamente en build/
     emptyOutDir: true, // limpia build antes de compilar
