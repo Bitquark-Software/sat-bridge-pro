a6b46043a7c3e04927bb4b9e6965a23e33d47765
Ing. Fernando <idsfernandomorales@gmail.com>
2025-08-14 13:53:25 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isLocalDevelopment = process.env.NODE_ENV === 'local'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isLocalDevelopment ? '/' : './',
+  base: '/',
   build: {
-    outDir: 'build', // <-- genera directamente en build/
-    emptyOutDir: true, // limpia build antes de compilar
+    outDir: 'build',
+    emptyOutDir: true,
   },
 })
