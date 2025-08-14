1cdcbdb219255472053d4951b71215cc95aa692f
Ing. Fernando <idsfernandomorales@gmail.com>
2025-08-14 13:50:16 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isLocalDevelopment = process.env.NODE_ENV === 'local'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sat-bridge-pro/',
+  base: isLocalDevelopment ? '/ ' : './',
   build: {
     outDir: 'build', // <-- genera directamente en build/
     emptyOutDir: true, // limpia build antes de compilar
