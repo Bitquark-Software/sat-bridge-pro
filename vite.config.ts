014c1dc6f435a1518fb107b5e14d5944b3ef601e
Ing. Fernando <idsfernandomorales@gmail.com>
2025-08-14 13:35:36 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isLocalDevelopment = process.env.NODE_ENV === 'local'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: isLocalDevelopment ? '/' : '/sat-bridge-pro/',
+  base: '/sat-bridge-pro/',
 })
